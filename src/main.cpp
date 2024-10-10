ad365914006859321fad7541931a7baec6ca6a45
josephwehby <josephw31@hotmail.com>
2024-10-10 15:45:41 -0500

    this program is so slow for large numbers. need a better way to do multiply and divide and mod

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,20 @@
 
 int main() {
   
-  BigInt a = bigintmath::BigIntFromString("10");
+  BigInt a = bigintmath::BigIntFromString("123456789");
   std::cout << "A: " << a << std::endl;
   
-  BigInt b = bigintmath::BigIntFromString("23");
+  BigInt b = bigintmath::BigIntFromString("12");
   std::cout << "B: " << b << std::endl;
 
-  BigInt c = bigintmath::BigIntFromString("7");
+  BigInt c = bigintmath::BigIntFromString("890");
   std::cout << "C: " << c << std::endl;
   
 
   std::cout << "A*B = " << bigintmath::Multiply(a, b) << std::endl;
   std::cout << "A/B = " << bigintmath::Divide(a, b) << std::endl;
   std::cout << "A^2 = " << bigintmath::Pow(a, 2) << std::endl;
-  std::cout << "A%B = " << bigintmath::Mod(a, b) << std::endl;
+  //std::cout << "A%B = " << bigintmath::Mod(a, b) << std::endl;
   std::cout << "A^B mod C = " << bigintmath::ModPow(a, b, c) << std::endl;
  
   return 0;
