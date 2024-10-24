b6f6657092ae6de5b9a93454ad9df835ce9d4057
josephwehby <josephw31@hotmail.com>
2024-10-24 11:47:44 -0500

    doing a rewrite to convert the namespace to a class

--- include/temp.hpp
+++ include/temp.hpp
@@ -6,6 +6,33 @@
 #include <string>
 #include <vector>
 
+struct BigInt {
+  std::vector<uint8_t> digits;
+  bool isNegative;
+  uint32_t size;
+
+  friend std::ostream& operator<<(std::ostream&, const BigInt&);
+};
+ 
+namespace bigintmath {
+  BigInt BigIntFromString(const std::string&);
+  BigInt BigIntFromInt(int);
+  BigInt BigIntFromBytes(const std::vector<uint8_t>&);
+  int Compare(const BigInt& a, const BigInt& b);
+  bool isZero(const BigInt& a);
+  bool isOne(const BigInt& a);
+  bool isEven(const BigInt& a);
+  bool gtZero(const BigInt& a);
+  BigInt Add(const BigInt& a, const BigInt& b);
+  BigInt Subtract(const BigInt& a, const BigInt& b);
+  BigInt Multiply(const BigInt& a, const BigInt& b);
+  BigInt Divide(const BigInt& a, const BigInt& b);
+  BigInt Mod(const BigInt& a, const BigInt& b);
+  BigInt Pow(const BigInt& a, const BigInt& power);
+  BigInt ModPow(const BigInt& a, const BigInt& b, const BigInt& c);
+  BigInt RightShift(const BigInt& a);
+  BigInt LeftShift(const BigInt& a);
+}
 
 class BigInt {
   public:
@@ -37,26 +64,3 @@ class BigInt {
     bool isNegative;
     uint32_t size;
 };
-
-
-/*
-namespace bigintmath {
-  BigInt BigIntFromString(const std::string&);
-  BigInt BigIntFromInt(int);
-  BigInt BigIntFromBytes(const std::vector<uint8_t>&);
-  int Compare(const BigInt& a, const BigInt& b);
-  bool isZero(const BigInt& a);
-  bool isOne(const BigInt& a);
-  bool isEven(const BigInt& a);
-  bool gtZero(const BigInt& a);
-  BigInt Add(const BigInt& a, const BigInt& b);
-  BigInt Subtract(const BigInt& a, const BigInt& b);
-  BigInt Multiply(const BigInt& a, const BigInt& b);
-  BigInt Divide(const BigInt& a, const BigInt& b);
-  BigInt Mod(const BigInt& a, const BigInt& b);
-  BigInt Pow(const BigInt& a, const BigInt& power);
-  BigInt ModPow(const BigInt& a, const BigInt& b, const BigInt& c);
-  BigInt RightShift(const BigInt& a);
-  BigInt LeftShift(const BigInt& a);
-}
-*/
--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,23 @@
 
 int main() {
   
-  BigInt a = bigintmath::BigIntFromInt(1234567);
+  BigInt a = BigInt(1234567);
   std::cout << "A: " << a << std::endl;
   
-  BigInt b = bigintmath::BigIntFromString("12");
+  BigInt b = BigInt("12");
   std::cout << "B: " << b << std::endl;
 
-  BigInt c = bigintmath::BigIntFromString("78956432");
+  BigInt c = BigInt("78956432");
   std::cout << "C: " << c << std::endl;
   
-  // bytes to bigint
+  /*
   std::vector<uint8_t> hello;
   std::string text = "hello";
   for (char c : text) hello.push_back(static_cast<uint8_t>(c));
   
-  BigInt byte_to_num = bigintmath::BigIntFromBytes(hello);
+  BigInt byte_to_num = BigInt(hello);
   std::cout << text << " : " << byte_to_num << std::endl;
   
-
-  // bigint math operations
   std::cout << "A+B = " << bigintmath::Add(a, b) << std::endl;
   std::cout << "A-B = " << bigintmath::Subtract(a, b) << std::endl;
   std::cout << "A/B = " << bigintmath::Divide(a, b) << std::endl;
@@ -31,6 +29,6 @@ int main() {
   std::cout << "A << 1  = " << bigintmath::LeftShift(a) << std::endl;
   std::cout << "A >> 1  = " << bigintmath::RightShift(a) << std::endl;
   std::cout << "A ^ B  = " << bigintmath::Pow(a, b) << std::endl;
-
+  */
   return 0;
 }
